815fdb095e74b10e2bee1c31c5a0bf1b194ea576
yasin.gulec94@outlook.com <yasin.gulec94@outlook.com>
2019-06-07 17:19:26 +0300

    -profile page, update profile

--- EventManagingSystem.BLL/PersonManager.cs
+++ EventManagingSystem.BLL/PersonManager.cs
@@ -12,15 +12,26 @@ namespace EventManagingSystem.BLL
     {
         DataContextModel dc = new DataContextModel();
         public void Register(Person person)
-        {
-            
+        {          
             dc.Person.Add(person);
             dc.SaveChanges();
         }
-
         public Person GetPerson(Person person)
         {
             return dc.Person.FirstOrDefault(p=>p.UserName==person.UserName && p.Password==person.Password);
+        }
+        public Person GetPerson(int id)
+        {
+            return dc.Person.FirstOrDefault(p => p.PersonID == id);
+        }
+        public void UpdateProfile(Person _person)
+        {
+            Person person = dc.Person.FirstOrDefault(p => p.PersonID == _person.PersonID);
+            person.FirstName = _person.FirstName;
+            person.DateOfBirth = _person.DateOfBirth;
+            person.UserName = _person.UserName;
+            person.Password = _person.Password;
+            dc.SaveChanges();
         }
     }
 }
---
+++ EventManagingSystem.UI/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using EventManagingSystem.BLL;
+using EventManagingSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventManagingSystem.UI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        PersonManager personManager = new PersonManager();
+        // GET: Profile
+        [HttpGet]
+        public ActionResult MyProfile()
+        {
+            Person person = (Person)Session["Login"];
+            var per = personManager.GetPerson(person.PersonID);
+            return View(per);
+        }
+        [HttpPost]
+        public ActionResult MyProfile(Person _person)
+        {
+            personManager.UpdateProfile(_person);
+            return View();
+        }
+    }
+}
