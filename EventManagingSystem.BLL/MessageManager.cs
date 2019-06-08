38aea057aed65a73a5ac42a2e3bcdca9ebd5d915
yasin.gulec94@outlook.com <yasin.gulec94@outlook.com>
2019-06-09 02:15:44 +0300

    features: message, not completed yet

---
+++ EventManagingSystem.BLL/MessageManager.cs
@@ -0,0 +1,29 @@
+﻿using EventManagingSystem.DAL;
+using EventManagingSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagingSystem.BLL
+{
+   public class MessageManager
+    {
+        DataContextModel dc = new DataContextModel();
+        
+        public List<Message> Inbox(int id)
+        {
+            return dc.Message.Where(m => m.MessageTo == id).ToList();
+        }
+        public List<Message> Outbox(int id)
+        {
+            return dc.Message.Where(m => m.MessageFrom == id).ToList();
+        }
+        public void SendMessage(Message message)
+        {
+            dc.Message.Add(message);
+            dc.SaveChanges();
+        }
+    }
+}
---
+++ EventManagingSystem.UI/Controllers/MessageController.cs
@@ -0,0 +1,43 @@
+﻿using EventManagingSystem.BLL;
+using EventManagingSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventManagingSystem.UI.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager messageManager = new MessageManager();
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            Person person =(Person) Session["Login"];
+            List<Message> messages = messageManager.Inbox(person.PersonID);
+            return View(messages);
+        }
+        public ActionResult Outbox()
+        {
+            Person person = (Person)Session["Login"];
+            List<Message> messages = messageManager.Outbox(person.PersonID);
+            return View(messages);
+        }
+        [HttpGet]
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendMessage(Message message)
+        {
+            Person person = (Person)Session["Login"];
+            message.MessageFrom = person.PersonID;
+            //kime gönderceğimizi seçmemiz gerek
+            message.MessageDate = DateTime.Now;
+            messageManager.SendMessage(message);
+            return View();
+        }
+    }
+}
