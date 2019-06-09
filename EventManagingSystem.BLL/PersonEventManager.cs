2a3c1d9a331ceb2f5a3720f321d00fe2a95ec791
yasin.gulec94@outlook.com <yasin.gulec94@outlook.com>
2019-06-10 00:54:30 +0300

    coding the join feature

---
+++ EventManagingSystem.BLL/PersonEventManager.cs
@@ -0,0 +1,20 @@
+﻿using EventManagingSystem.DAL;
+using EventManagingSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagingSystem.BLL
+{
+ public class PersonEventManager
+    {
+        DataContextModel dc = new DataContextModel();
+        public void Join(EventPerson eventPerson)
+        {
+            dc.EventPerson.Add(eventPerson); 
+            dc.SaveChanges();
+        }
+    }
+}
---
+++ EventManagingSystem.UI/Controllers/JoinController.cs
@@ -0,0 +1,32 @@
+﻿using EventManagingSystem.BLL;
+using EventManagingSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventManagingSystem.UI.Controllers
+{
+    public class JoinController : Controller
+    {
+        PersonEventManager eventManager = new PersonEventManager();
+        // GET: Join
+        [HttpGet]
+        public ActionResult JoinEvent(int id)
+        {
+            Session["EventID"] = id;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult JoinEvent(EventPerson eventPerson)
+        {
+            Person per =(Person) Session["Login"];
+            eventPerson.PersonID = per.PersonID;
+            eventPerson.EventID = (int)Session["EventID"];
+            eventPerson.TotalPerson = eventPerson.TotalPerson + 1;
+            eventManager.Join(eventPerson);
+            return View();
+        }
+    }
+}
